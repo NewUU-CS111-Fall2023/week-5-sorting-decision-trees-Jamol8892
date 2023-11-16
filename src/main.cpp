20a8937007a4883c11e6a2cc767b41a35953ccc9
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-11-16 22:01:03 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,60 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
-#include <iostream>
 #include "task_1.h"
+#include "task_2.h"
+#include "task_3.h"
+#include "task_4.h"
+
+using namespace Task3;
+using namespace Task4;
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
-    return 0;
+    cout << "Task 1: " << endl;
+    vector<string> bookTitles = {
+            "Catcher in the Rye",
+            "Pride and Prejudice",
+            "To Kill a Mockingbird",
+            "The Great Gatsby",
+            "Moby Dick"
+    };
+
+    bubbleSort(bookTitles);
+
+    for (const auto &title: bookTitles) {
+        cout << title << endl;
+    }
+        cout << "Task 2: " << endl;
+
+    int numDenominations;
+    cout << "Enter the number of denominations: ";
+    cin >> numDenominations;
+
+    vector<int> denominations(numDenominations);
+    cout << "Enter the denominations:\n";
+    for (int i = 0; i < numDenominations; ++i) {
+        cin >> denominations[i];
+    }
+
+    Task2::selectionSort(denominations);
+
+    Task2::displaySortedDenominations(denominations);
+
+        cout << "Task 3: " << endl;
+
+    vector<string> soldiers = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
+
+    rearrangeSoldiers(soldiers);
+
+    cout << "Rearranged soldiers:\n";
+    for (const auto& soldier : soldiers) {
+        cout << soldier << "\n";
+    }
+
+        cout << "Task 4: " << endl;
+
+    vector<int> arr = {5, 2, 8, 1, 4, 6};
+
+    int result = countElementsWithSmallerNeighbors(arr);
+    cout << result << "\n";
+
+        cout << "Task 5: " << endl;
+
 }

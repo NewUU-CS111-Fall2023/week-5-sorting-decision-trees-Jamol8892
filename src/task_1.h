ced277ac5f4723c7f24b59784c536f6abcec34ac
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-11-16 22:01:21 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,14 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+// task_1.h
+
+#ifndef TASK_1_H
+#define TASK_1_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void bubbleSort(vector<string>& titles);
+
+#endif // TASK_1_H

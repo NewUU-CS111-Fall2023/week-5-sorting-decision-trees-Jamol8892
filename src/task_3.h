b27663e40b6fedd2601ebfb20eaf0c3ac968cc27
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-11-16 22:02:36 +0500

    Create task_3.h

---
+++ src/task_3.h
@@ -0,0 +1,11 @@
+#ifndef TASK_3_H
+#define TASK_3_H
+
+#include <vector>
+#include <iostream>
+
+namespace Task3 {
+    void rearrangeSoldiers(std::vector<std::string>& soldiers);
+}
+
+#endif  // TASK_3_H

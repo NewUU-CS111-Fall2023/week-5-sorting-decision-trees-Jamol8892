8021f0eadd6fe73a3610f9424ee4c36ce621104d
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-11-16 22:02:00 +0500

    Create task_2.h

---
+++ src/task_2.h
@@ -0,0 +1,11 @@
+#ifndef TASK_2_H
+#define TASK_2_H
+
+#include <vector>
+
+namespace Task2 {
+    void selectionSort(std::vector<int>& denominations);
+    void displaySortedDenominations(const std::vector<int>& denominations);
+}
+
+#endif
